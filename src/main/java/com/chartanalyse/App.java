714c0d6681f8a9e05891bc2f31d1536b6ac5349c
ErzHerzO6 <macgyver1fl@gmail.com>
2026-02-18 12:59:33 +0100

    initializing the prodjekt

---
+++ src/main/java/com/chartanalyse/App.java
@@ -0,0 +1,8 @@
+package com.chartanalyse;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Chart Analyse - Aktienmarkt Analysediagramm");
+        System.out.println("Projekt erfolgreich gestartet!");
+    }
+}
